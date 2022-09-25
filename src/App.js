27f1919a2911a366372a65f5f4d1bd7f9994e665
JohnQ1981 <ikram1981@gmail.com>
2022-09-24 23:29:51 -0500

    better styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SeasonDisplay from './components/SeasonDisplay';
 import Loader from './components/Loader';
+
 // const App = ()=>{
 //     window.navigator.geolocation.getCurrentPosition(
 //         (position)=> console.log(position),
@@ -39,32 +40,39 @@ state = { lat: null, errorMessage: "" };
       (err) => this.setState({ errorMessage: err.message })
     );
   }
+  renderContent(){
+    if ((this.state.errorMessage && !this.state.lat)) {
+      return <div>Error: {this.state.errorMessage}</div>
+     } else if ((this.state.lat && !this.errorMessage)&&(this.state.lat > 0)) {
+      return <div>Latitude: {this.state.lat} and it is in Northern Hemisphere
+          <div><SeasonDisplay
+                  lat ={this.state.lat}
+
+                  />
+          </div>
+      </div>
+     } 
+     else if ((this.state.lat && !this.errorMessage)&&(this.state.lat < 0)) {
+      return <div>Latitude: {this.state.lat} and it is in Southern Hemisphere
+      <div><SeasonDisplay
+                  lat ={this.state.lat}
+
+                  />
+          </div>
+      </div>
+     }
+     else {
+     return <div><Loader
+     message='Please allow app to access to your location' /></div>
+     }
+
+  }
 
     render(){
-                
-       if ((this.state.errorMessage && !this.state.lat)) {
-        return <div>Error: {this.state.errorMessage}</div>
-       } else if ((this.state.lat && !this.errorMessage)&&(this.state.lat > 0)) {
-        return <div>Latitude: {this.state.lat} and it is in Northern Hemisphere
-            <div><SeasonDisplay
-                    lat ={this.state.lat}
-
-                    />
-            </div>
-        </div>
-       } 
-       else if ((this.state.lat && !this.errorMessage)&&(this.state.lat < 0)) {
-        return <div>Latitude: {this.state.lat} and it is in Southern Hemisphere
-        <div><SeasonDisplay
-                    lat ={this.state.lat}
-
-                    />
-            </div>
-        </div>
-       }
-       else {
-       return <div><Loader
-       message='Please allow app to access to your location' /></div>}
+              return(
+                <div className='border red'>{this.renderContent()}</div>
+              ) ; 
+       
     }
 }
 
